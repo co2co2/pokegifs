f9e30671f427371e0f9d445cdf725bb23129ee21
co2co2 <coco8287@hotmail.com>
2017-12-17 23:15:57 -0500

    finally done

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -5,9 +5,9 @@ class PokemonController < ApplicationController
     body = JSON.parse(res.body)
     name  = body["name"]
     id = body["id"]
-    types = body["types"][0]["name"]
+    types = body["types"][0]["type"]["name"]
 
-    res = HTTParty.get("https://api.giphy.com/v1/gifs/search?search?api_key=#{Rails.application.secrets.pokemon_key}&q=#{name}&rating=g")
+    res = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{Rails.application.secrets.pokemon_key}&q=#{name}&rating=g")
     body = JSON.parse(res.body)
     gif_url = body["data"][0]["url"]
 
@@ -15,6 +15,8 @@ class PokemonController < ApplicationController
       "id": id,
       "name": name,
       "types": types,
-      "gif" : gif_url
+      "gif": gif_url
 }
+  end
+
 end
