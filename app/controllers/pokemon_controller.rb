2e4cb5dba3f58558fd8fdd60a208326f2980a5b0
co2co2 <coco8287@hotmail.com>
2017-12-17 22:56:16 -0500

    sth wrong with git hub again

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -7,11 +7,14 @@ class PokemonController < ApplicationController
     id = body["id"]
     types = body["types"][0]["name"]
 
-    res = HTTParty.get("https://api.giphy.com/v1/gifs/search?search?api_key=#{Rails.application.secrets.pokemon_key}&q=bulbasaur&rating=g")
+    res = HTTParty.get("https://api.giphy.com/v1/gifs/search?search?api_key=#{Rails.application.secrets.pokemon_key}&q=#{name}&rating=g")
     body = JSON.parse(res.body)
     gif_url = body["data"][0]["url"]
 
-    render json: { "message": "ok" }
-
-  end
+    render json: {
+      "id": id,
+      "name": name,
+      "types": types,
+      "gif" : gif_url
+}
 end
